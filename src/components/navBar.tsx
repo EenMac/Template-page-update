e831eca0062b7fe085a4dd76e9e1fef5899e117c
EenTwee <98649861+EenTwee@users.noreply.github.com>
2023-02-08 23:24:07 +0000

    Navbar component created and successfully rendered

---
+++ src/components/navBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "../app/page.module.css"
+
+function NavBar() {
+  return (
+    <div className={styles.navBar}>
+        <h1>Navbar components</h1>
+    </div>
+  )
+}
+
+export default NavBar;
