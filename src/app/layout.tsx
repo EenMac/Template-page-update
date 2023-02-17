a8724741ec66720aa3a82c9866d4b1ece97ab854
EenTwee <98649861+EenTwee@users.noreply.github.com>
2023-02-17 22:23:26 +0000

    redundant head removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import NavBar from '@/components/navBar'
 
 export default function RootLayout({
   children,
@@ -7,11 +8,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
+      <NavBar/>
       <body>{children}</body>
     </html>
   )
