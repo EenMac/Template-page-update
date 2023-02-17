eb113f727381ad2081e6ebac5d1e214beb70d941
EenTwee <98649861+EenTwee@users.noreply.github.com>
2023-02-17 22:23:49 +0000

    Navbar moved to top level component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
-import NavBar from '@/components/navBar'
 import About from '@/components/about'
 import Experiences from '@/components/experiences'
 import Contact from '@/components/contact'
@@ -13,7 +12,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <NavBar/>
         <About/>
         <Experiences/>
         <Contact/>
